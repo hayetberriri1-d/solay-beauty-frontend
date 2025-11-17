2ca09690853f88cda4e7ccbdf03190de3480a1a4
Hayet Berriri <hayetberriri1@gmail.com>
2025-11-17 12:07:25 +0100

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/solay-beauty-frontend/", // <-- ajouter cette ligne
   server: {
     host: "::",
     port: 8080,
